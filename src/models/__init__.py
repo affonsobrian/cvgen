57999545a88f4b373cbbbe036effc5d2dc6915ea
Affonso Brian Pereira Azevedo <contato.affonsobrian@gmail.com>
2020-02-06 23:28:05 -0300

    Fix __init__.py

--- src/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This code was generated automatically by the Builder
-"""
-from .Curriculum import Curriculum
-from .ReadableFields import ReadableFields
-from .ProfessionalExperience import ProfessionalExperience
-from .Address import Address
-from .PersonalData import PersonalData

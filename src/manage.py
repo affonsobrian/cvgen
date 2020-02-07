57aa0d4d0712bfeceab8595284ae1a4c14cc3be5
Affonso Brian Pereira Azevedo <contato.affonsobrian@gmail.com>
2020-02-06 23:26:25 -0300

    Modify the manage.py to be able to create also the dictionary from __init__.py

--- src/manage.py
+++ src/manage.py
@@ -1,14 +1,24 @@
 import argparse
 import os
+import re
 from os import listdir
 from os.path import isfile, join
 from main import main
 
-def build_models_init():
-    path = os.path.abspath(__file__)
-    path = path.replace(__file__, 'models')
+
+def snake_case(value):
+    """
+    Convert camel case to snake case.
+    i.e.: CamelCase turns to camel_case
+    """
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', value).lower() 
+
+
+def get_models_files(path):
+    """
+    Returns a list with the names of the models files
+    """
     not_allowed_files = ('__init__.py', '__init__.pyc',)
-    files = [f for f in listdir(path) if isfile(join(path, f))]
     files = []
     for f in listdir(path):
         if not isfile(join(path, f)):
@@ -17,13 +27,28 @@ def build_models_init():
             continue
         f_save_name = f.replace('.py', '')
         files.append(f_save_name)
+    return files
+
+
+def build_models_init(path, files):
+    """
+    Prepares the __init__.py from the models package
+    """
     comment = '"""\nThis code was generated automatically by the Builder\n"""\n'
     with open(f'{path}/__init__.py', 'w') as open_file:
         open_file.write(comment)
         for f in files:
             open_file.write(f"from .{f} import {f}\n")
-
+        open_file.write('\n\nvariables = {')
+        last = len(files) - 1
+        for index, f in enumerate(files):
+            open_file.write(f"\n    '{snake_case(f)}': {f}")
+            if index == last:
+                continue
+            open_file.write(f",")
+        open_file.write("\n}")
     print('Builded with success')
+
 
 parser = argparse.ArgumentParser()
 optional = parser.add_argument_group("optional arguments")
@@ -32,13 +57,15 @@ optional.add_argument(
 optional.add_argument(
     "-r", "--run", help="run the project",  action="store_true")
 
-
 args = parser.parse_args()
-print(args)
 
 if not args:
     raise Exception("You must pass a command, type --help or -h to see")
 if args.build:
-    build_models_init()
+    path = os.path.abspath(__file__)
+    path = path.replace(__file__, 'models')
+    files = get_models_files(path)  
+    build_models_init(path, files)
 elif args.run:
     main()
+
